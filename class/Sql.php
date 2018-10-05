89fb95c1eff05853bfd6e790d7ecc8d75ca8e8c5
mathitz <matheusalves789@gmail.com>
2018-10-04 23:01:13 -0300

    Data Acess Object - Usos

--- class/Sql.php
+++ class/Sql.php
@@ -1,57 +1,52 @@
-<?php
+<?php 
 
-class Sql extends PDO
-{
+class Sql extends PDO {
 
-    private $conn;
+	private $conn;
 
-    public function __construct()
-    {
-        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "Denise280600");
+	public function __construct(){
 
-    }
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "Denise280600");
 
-    private function setParams($statment, $parameters = array())
-    {
-        foreach ($parameters as $key => $value)
-        {
+	}
 
-            $this->setParam($key, $value);
-        }
+	private function setParams($statement, $parameters = array()){
 
-    }
+		foreach ($parameters as $key => $value) {
+			
+			$this->setParam($statement, $key, $value);
 
-    private function setParam($statment, $key, $value)
-    {
+		}
 
-        $statement->bindParam($key, $value);
+	}
 
-    }
+	private function setParam($statement, $key, $value){
 
+		$statement->bindParam($key, $value);
 
-    public function query($rawQuery, $params = array())
-    {
-        
-        $stmt = $this->conn->prepare($rawQuery);
+	}
 
-        $this->setParams($stmt, $params);
-        
-        $stmt->execute();
+	public function query($rawQuery, $params = array()){
 
-        return $stmt;
-    }
+		$stmt = $this->conn->prepare($rawQuery);
 
-    public function select($rawQuery, $params = array()):array
-    {
+		$this->setParams($stmt, $params);
 
-        $stmt = $this->query($rawQuery, $params);
+		$stmt->execute();
 
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $stmt;
 
-    }
+	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->query($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	}
 
 }
 
-
-
-?>
+ ?>
--- index.php
+++ index.php
@@ -1,11 +1,47 @@
-<?php
+<?php 
 
 require_once("config.php");
 
-$sql = new Sql();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$usuarios = $sql->select("SELECT * FROM tb_usuario");
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo json_encode($usuarios);
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
-?>
+//carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+
+//echo $usuario;
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+/*
+//Alterar um usuário
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
+ ?>
